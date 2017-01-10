02e056e2cfeb57f850ad36af63a264515f34e5b9
Buddhika <Buddhika@LAPTOP-659KAUKK>
2017-01-10 10:50:48 +0530

    Added ApplicationUserViewModel

---
+++ ViewModels/FormViewModels/ApplicationUserViewModel.cs
@@ -0,0 +1,29 @@
+﻿namespace ViewModels.FormViewModels
+{
+    public class ApplicationUserViewModel
+    {
+        public int UserIdGenerated { get; set; }
+        public string UserId { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string AccessFailedCount { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JoinDate { get; set; }
+        public string ContractEndDate { get; set; }
+        public string UserActive { get; set; }
+        public string UserLocked { get; set; }
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string AddressLine3 { get; set; }
+        public string CountyOrRegion { get; set; }
+        public string Country { get; set; }
+        public string Postcode { get; set; }
+        public string VolunteerInterests { get; set; }
+        public string LastLoginDateTime { get; set; }
+        public string ContactTypeId { get; set; }
+        public string RoleId { get; set; }
+        public string RoleName { get; set; }
+    }
+}
